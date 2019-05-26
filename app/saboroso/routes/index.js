e30268287ff444c6aff8bfc9b966b9f57ec77f14
Artur Killdragon <artur.guitelar@gmail.com>
2019-05-26 16:17:16 -0300

    Pre-configuração de rotas principais.

--- app/saboroso/routes/index.js
+++ app/saboroso/routes/index.js
@@ -20,4 +20,36 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/** Contacts */
+router.get('/contacts', function(req, res, next) {
+  
+  res.render('contacts', {
+    title: 'Contatos - Restaurante Saboroso!'
+  });
+});
+
+/** Menus */
+router.get('/menus', function(req, res, next) {
+  
+  res.render('menus', {
+    title: 'Menus - Restaurante Saboroso!'
+  });
+});
+
+/** Reservations */
+router.get('/reservations', function(req, res, next) {
+  
+  res.render('reservations', {
+    title: 'Reservas - Restaurante Saboroso!'
+  });
+});
+
+/** Services */
+router.get('/services', function(req, res, next) {
+  
+  res.render('services', {
+    title: 'Serviços - Restaurante Saboroso!'
+  });
+});
+
 module.exports = router;
