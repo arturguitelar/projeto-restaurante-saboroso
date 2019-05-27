1f3965cfa0ea1b1638a38d346e4107c552717b9c
Artur Killdragon <artur.guitelar@gmail.com>
2019-05-27 00:03:30 -0300

    Create: Rotas para urls admin. Arquivos de layout da área admin movidos para pasta view.

---
+++ app/saboroso/routes/admin.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+/** Home */
+router.get('/', function(req, res, next) {
+
+    res.render('admin/index');
+});
+
+/** Login */
+router.get('/login', function(req, res, next) {
+
+    res.render('admin/login');
+});
+
+/** Contacts */
+router.get('/contacts', function(req, res, next) {
+
+    res.render('admin/contacts');
+});
+
+/** Emails */
+router.get('/emails', function(req, res, next) {
+
+    res.render('admin/emails');
+});
+
+/** Menus */
+router.get('/menus', function(req, res, next) {
+
+    res.render('admin/menus');
+});
+
+/** Reservations */
+router.get('/reservations', function(req, res, next) {
+
+    res.render('admin/reservations', {
+        date: {}
+    });
+});
+
+/** Users */
+router.get('/users', function(req, res, next) {
+
+    res.render('admin/users');
+});
+
+module.exports = router;
