74bc1f9f35adfc60fa748435c20b458d2a4ca2f5
Artur Killdragon <artur.guitelar@gmail.com>
2019-05-31 00:14:28 -0300

    Admin > users: CRUD na tela de usuários. Refact: método 'onUpdateLoad' agora é padrão na classe 'HcodeGrid'.

--- app/saboroso/inc/users.js
+++ app/saboroso/inc/users.js
@@ -46,5 +46,90 @@ module.exports = {
                 }
             });
         });
+    },
+
+    /**
+     * @return Usuários ordenados por nome.
+     */
+    getUsers() {
+
+        return new Promise((resolve, reject) => {
+
+            conn.query("SELECT * FROM tb_users ORDER BY name", (err, results) => {
+
+                if (err) {
+                    reject(err);
+                } else {
+            
+                    resolve(results);
+                }
+            });
+        });
+    },
+
+    /**
+     * @param {*} fields Campos do fomulário.
+     * @param {*} files Aquivos enviados.
+     * @returns Registro salvo no banco de dados.
+     */
+    save(fields, files) {
+
+        return new Promise((resolve, reject) => {
+
+            let query, params = [
+                fields.name,
+                fields.email
+            ];
+
+            if (fields.id > 0) {
+                // neste caso, será um update
+                params.push(fields.id);
+
+                query = `
+                    UPDATE tb_users
+                    SET name = ?,
+                        email = ?
+                    WHERE id = ?
+                `;
+            } else {
+                // neste caso, será um insert
+                query = `
+                    INSERT INTO tb_users (name, email, password)
+                    VALUES (?, ?, ?)
+                `;
+
+                params.push(fields.password);
+            }
+
+            conn.query(query , params, (err, results) => {
+
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    },
+
+    /**
+     * @param {*} id Id do registro que será deletado.
+     * @returns Registro deletado.
+     */
+    delete(id) {
+        
+        return new Promise((resolve, reject) => {
+
+            conn.query(`
+                DELETE FROM tb_users WHERE id = ?
+            `, [ id ], (err, results) => {
+
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
     }
 }
