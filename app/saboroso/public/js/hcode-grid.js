888d3cc2b2f2afd984e96e7951f6bf5b428f6055
Artur Killdragon <artur.guitelar@gmail.com>
2019-05-30 23:08:36 -0300

    Refact: Botão delete e eventos before e after do formulário.

--- app/saboroso/public/js/hcode-grid.js
+++ app/saboroso/public/js/hcode-grid.js
@@ -8,9 +8,24 @@ class HcodeGrid {
         configs.listeners = Object.assign({
             afterUpdateClick: e => {
                 $('#modal-update').modal('show');
+            },
+            afterDeleteClick: e => {
+                window.location.reload();
+            },
+            afterFormCreate: e => {
+                window.location.reload();
+            },
+            afterFormUpdate: e => {
+                window.location.reload();
+            },
+            afterFormCreateError: e => {
+                alert('Não foi possível enviar o formulário.');
+            },
+            afterFormUpdateError: e => {
+                alert('Não foi possível enviar o formulário.');
             }
         }, configs.listeners);
-        
+
         this.options = Object.assign({}, {        
             formCreate: '#modal-create form',
             formUpdate: '#modal-update form',
@@ -27,31 +42,30 @@ class HcodeGrid {
         this.formCreate = document.querySelector(this.options.formCreate);
 
         this.formCreate.save().then(json => {
-            window.location.reload();
+            this.fireEvent('afterFormCreate');
         }).catch(err => {
-            console.error(err);
+            this.fireEvent('afterFormCreateError');
         });
 
         this.formUpdate = document.querySelector(this.options.formUpdate);
 
         this.formUpdate.save().then(json => {
-            window.location.reload();
+            this.fireEvent('afterFormUpdate');
         }).catch(err => {
-            console.error(err);
+            this.fireEvent('afterFormUpdateError');
         });
     }
 
     initButtons() {
 
+        // btns update
         [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
 
             btn.addEventListener('click', e => {
-                
-                let tr = e.path.find(el => {
-                    return (el.tagName.toUpperCase() === 'TR');
-                });
 
-                let data = JSON.parse(tr.dataset.row);
+                this.fireEvent('beforeUpdateClick', [e]);
+                
+                let data = this.getTrData(e);
                 
                 for (let name in data) {
 
@@ -67,19 +81,18 @@ class HcodeGrid {
                     }
                 }
 
-                this.fireEvents('afterUpdateClick', [e]);
+                this.fireEvent('afterUpdateClick', [e]);
             });
         });
 
+        // btns delete
         [...document.querySelectorAll(this.options.btnDelete)].forEach(btn => {
 
             btn.addEventListener('click', e => {
 
-                let tr = e.path.find(el => {
-                    return (el.tagName.toUpperCase() === 'TR');
-                });
+                this.fireEvent('beforeDeleteClick');
 
-                let data = JSON.parse(tr.dataset.row);
+                let data = this.getTrData(e);
 
                 // janela de confirmação
                 if (confirm(eval('`' + this.options.deleteMsg + '`'))) {
@@ -89,18 +102,27 @@ class HcodeGrid {
                     })
                         .then(response => response.json())
                         .then(json => {
-                            window.location.reload();
+                            this.fireEvent('afterDeleteClick');
                         });
                 }
             });
         });
     }
 
+    getTrData(e) {
+
+        let tr = e.path.find(el => {
+            return (el.tagName.toUpperCase() === 'TR');
+        });
+
+        return JSON.parse(tr.dataset.row);
+    }
+
     /**
      * @param {String} name Nome da função.
      * @param {Array} args Argumentos da função.
      */
-    fireEvents(name, args) {
+    fireEvent(name, args) {
         if (this.options.listeners[name] && typeof this.options.listeners[name] === 'function')
             this.options.listeners[name].apply(this, args);
     }
